c960a9952de26116ce4cb09470b9c14a29f6a1a8
egoing <egoing@gmail.com>
2018-10-17 09:21:19 +0900

    조건문

---
+++ ex/conditional.js
@@ -0,0 +1,13 @@
+// Boolean 
+console.log(true);
+console.log(false);
+// Comparison operator 
+console.log(1 < 2);
+// Conditional Statements
+console.log(1);
+if(true){
+    console.log('2-1');
+} else {
+    console.log('2-2');
+}
+console.log(3);
