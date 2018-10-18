6076c6aeb0dba5b41ed11208269d1e59ef437056
egoing <egoing@gmail.com>
2018-10-18 16:36:03 +0900

    함수 적용

---
+++ ex/function.js
@@ -0,0 +1,9 @@
+console.log(Math.random());
+console.log(Math.floor(1.9));
+
+function sum(left, right){
+    var r = left + right;
+    return r;
+}
+var result = sum(1,4);
+console.log(result);
--- main.js
+++ main.js
@@ -2,6 +2,39 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+
+function templateList(){
+    var topics = fs.readdirSync('data');
+    var i = 0;
+    var listTag = '';
+    while(i < topics.length){
+        listTag = listTag + `<li><a href="/?id=${topics[i]}">${topics[i]}</a></li>`;
+        i = i + 1;
+    }
+    return listTag;
+}
+
+function templateHTML(_listTag, _title, _desc){
+    var content = `
+            <!doctype html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>WEB</title>
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                        ${_listTag}
+                    </ul>
+                    <h2>${_title}</h2>
+                    ${_desc}
+                </body>
+            </html>
+        `;
+    return content;
+}
+
 var server = http.createServer(
      function(request, response){
          
@@ -21,31 +54,9 @@ var server = http.createServer(
         
 //         console.log(desc);
 
-        var topics = fs.readdirSync('data');
-        var i = 0;
-        var listTag = '';
-        while(i < topics.length){
-            listTag = listTag + `<li><a href="/?id=${topics[i]}">${topics[i]}</a></li>`;
-            i = i + 1;
-        }
+        var listTag = templateList();
+        var content = templateHTML(listTag, title, desc);
 
-        var content = `
-            <!doctype html>
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                    <title>WEB</title>
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ul>
-                        ${listTag}
-                    </ul>
-                    <h2>${title}</h2>
-                    ${desc}
-                </body>
-            </html>
-        `;
         response.write(content);
         response.end();
      }
