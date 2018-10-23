1a8cb9f6afc491e3a6535485c2620080887e42ac
egoing <egoing@gmail.com>
2018-10-23 17:02:23 +0900

    apply express

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-// // // Nodejs야 니가 기본적으로 가지고 있는 기능 중에서 http 모듈을 가져와봐! 
+// // // // Nodejs야 니가 기본적으로 가지고 있는 기능 중에서 http 모듈을 가져와봐! 
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-function templateList(){
+function templateList() {
     var topics = fs.readdirSync('data');
     var i = 0;
     var listTag = '';
-    while(i < topics.length){
+    while (i < topics.length) {
         listTag = listTag + `<li><a href="/?id=${topics[i]}">${topics[i]}</a></li>`;
         i = i + 1;
     }
     return listTag;
 }
-
-function templateHTML(_listTag, _title, _desc){
+//parameter
+function templateHTML(_listTag, _title, _desc) {
     var content = `
             <!doctype html>
             <html>
@@ -36,29 +36,28 @@ function templateHTML(_listTag, _title, _desc){
 }
 
 var server = http.createServer(
-     function(request, response){
-         
-//         // favicon.icon는 좀 무시해!
-        if(request.url == '/favicon.ico'){
+    function (request, response) {
+
+//         //         // favicon.icon는 좀 무시해!
+        if (request.url == '/favicon.ico') {
             return response.writeHead(404);
         }
         var parsed_url = url.parse(request.url, true);
-        
-        if(request.url === '/') {
-            var title = 'Welcome';
+
+        if (request.url === '/') {
+            var title = 'Hi'; 
             var desc = 'Hello, web';
         } else {
+
             var title = parsed_url.query.id;
-            var desc = fs.readFileSync('data/'+parsed_url.query.id, 'utf8');
+            var desc = fs.readFileSync('data/' + parsed_url.query.id, 'utf8');
         }
-        
-//         console.log(desc);
 
         var listTag = templateList();
         var content = templateHTML(listTag, title, desc);
 
         response.write(content);
         response.end();
-     }
+    }
 );
 server.listen(3000);
