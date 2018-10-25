cf77ff1ca913b18a4ac385967eed2c893e0ba415
egoing <egoing@gmail.com>
2018-10-25 09:20:38 +0900

    다중 사용자 시스템으로 개선

--- express.js
+++ express.js
@@ -11,8 +11,33 @@ server.use(session({
     cookie: {
         secure: false
     },
-    store:new FileStore()
+    store: new FileStore()
 }));
+
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+server.use(passport.initialize());
+server.use(passport.session());
+passport.serializeUser(function (username, done) {
+    console.log('serializeUser', username);
+    done(null, username);
+});
+passport.deserializeUser(function (username, done) {
+    console.log('deserializeUser', username);
+    done(null, authData2[username]);
+});
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        console.log('LocalStrategy', username, password, authData2[username]);
+        if(authData2[username]){
+            if(authData2[username].password === password){
+                return done(null, username);
+            }
+        }
+        return done(null, false);
+    }
+));
+
 server.use(bodyParser.urlencoded({
     extended: false
 }));
@@ -21,7 +46,16 @@ var authData = {
     password: '111111',
     displayName: 'egoing'
 }
-
+var authData2 = {
+    'a@a.com':{
+        password:'a',
+        displayName:'AAA'
+    },
+    'b@b.com':{
+        password:'b',
+        displayName:'BBB'
+    }
+}
 function templateList() {
     var topics = fs.readdirSync('data');
     var i = 0;
@@ -35,7 +69,7 @@ function templateList() {
 //parameter
 function templateHTML(_listTag, _title, _desc, _displayName) {
     var authUI = '<a href="/login">login</a>';
-    if(_displayName){
+    if (_displayName) {
         authUI = '<a href="/logout_process">logout</a>';
     }
     var content = `
@@ -59,12 +93,14 @@ function templateHTML(_listTag, _title, _desc, _displayName) {
         `;
     return content;
 }
-
 server.get('/', function (request, response) {
     var title = 'Hi';
     var desc = 'Hello, web';
     var listTag = templateList();
-    var content = templateHTML(listTag, title, desc, request.session.displayName);
+    var displayName = null;
+    if(request.user)
+        displayName = request.user.displayName;
+    var content = templateHTML(listTag, title, desc, displayName);
     response.write(content);
     response.end();
 });
@@ -78,7 +114,10 @@ server.get('/login', function (request, response) {
         </form>
     `;
     var listTag = templateList();
-    var content = templateHTML(listTag, title, desc, request.session.displayName);
+    var displayName = null;
+    if(request.user)
+        displayName = request.user.displayName;
+    var content = templateHTML(listTag, title, desc, displayName);
     response.write(content);
     response.end();
 });
@@ -86,12 +125,15 @@ server.get('/topic/:title', function (request, response) {
     var title = request.params.title;
     var desc = fs.readFileSync('data/' + title, 'utf8');
     var listTag = templateList();
-    var content = templateHTML(listTag, title, desc, request.session.displayName);
+    var displayName = null;
+    if(request.user)
+        displayName = request.user.displayName;
+    var content = templateHTML(listTag, title, desc, displayName);
     response.write(content);
     response.end();
 });
 server.get('/create', function (request, response) {
-    if(!request.session.isLogin){
+    if (!request.user) {
         return response.redirect('/login');
     }
     var title = 'Create';
@@ -103,12 +145,15 @@ server.get('/create', function (request, response) {
         </form>
     `;
     var listTag = templateList();
-    var content = templateHTML(listTag, title, desc, request.session.displayName);
+    var displayName = null;
+    if(request.user)
+        displayName = request.user.displayName;
+    var content = templateHTML(listTag, title, desc, displayName);
     response.write(content);
     response.end();
 });
 server.post('/create_process', function (request, response) {
-    if(!request.session.isLogin){
+    if (!request.user) {
         return response.redirect('/login');
     }
     var title = request.body.title;
@@ -116,6 +161,14 @@ server.post('/create_process', function (request, response) {
     fs.writeFileSync(`data/${title}`, desc);
     response.redirect('/topic/' + title);
 });
+server.post('/login_process',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    })
+);
+
+/*
 server.post('/login_process', function (request, response) {
     var username = request.body.username;
     var password = request.body.password;
@@ -127,17 +180,19 @@ server.post('/login_process', function (request, response) {
         response.send('뉘신지?');
     }
 });
-server.get('/logout_process', function(request, response){
-    request.session.destroy(function(err){
+*/
+
+server.get('/logout_process', function (request, response) {
+    request.session.destroy(function (err) {
         response.redirect('/');
     });
 });
-server.get('/session/set', function(request, response){
+server.get('/session/set', function (request, response) {
     var rand = Math.random();
     request.session.rand = rand;
     response.send(rand.toString());
 });
-server.get('/session/get', function(request, response){
+server.get('/session/get', function (request, response) {
     response.send(request.session.rand.toString());
 });
 server.listen(3000);
